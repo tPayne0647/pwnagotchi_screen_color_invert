a08700fb5b99c48d7756b3bec564a65168930bb2
tPayne0647 <tristan.payne@northerndata.de>
2023-12-23 22:12:38 -0500

    add single and long press

--- pwnagotchi_scr_inv.py
+++ pwnagotchi_scr_inv.py
@@ -11,12 +11,20 @@ def send_pisugar_command_uds(command, socket_path="/tmp/pisugar-server.sock"):
         print(f"Socket error: {e}")
         return None
 
-# Example usage
+# Testing
+
 # Enable the double press button
 enable_response = send_pisugar_command_uds("set_button_enable double 1")
 print(enable_response)
 
+# Set the action for the single press button
+set_shell_response = send_pisugar_command_uds("set_button_shell single sudo /home/pi/pwnagotchi_screen_color_invert/invert_colors.sh")
+print(set_shell_response)
+
 # Set the action for the double press button
-# Replace the path with the actual path to your script
 set_shell_response = send_pisugar_command_uds("set_button_shell double sudo /home/pi/pwnagotchi_screen_color_invert/invert_colors.sh")
+print(set_shell_response)
+
+# Set the action for the long press button
+set_shell_response = send_pisugar_command_uds("set_button_shell long sudo /home/pi/pwnagotchi_screen_color_invert/invert_colors.sh")
 print(set_shell_response)
