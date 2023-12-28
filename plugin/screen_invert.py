c31cdf5a81767640afaf1caec0a37a7dfafe5a8a
tPayne0647 <tristan.payne@northerndata.de>
2023-12-28 10:58:18 -0500

    put client back in plugin file

--- plugin/screen_invert.py
+++ plugin/screen_invert.py
@@ -8,11 +8,62 @@ from pathlib import Path
 import pwnagotchi.plugins as plugins
 import pwnagotchi.ui.web as web
 
-home_dir = Path.home()
-client_path = home_dir / '~/pwnagotchi_screen_color_invert/client'
-sys.path.insert(0, str(client_path))
-from screen_invert import PiSugarClient
-sys.path.remove(str(client_path))
+class PiSugarClient:
+    """
+    PiSugarClient provides an interface to send commands to the PiSugar
+    server via a UNIX socket. It handles connecting to the server, clearing
+    the socket, sending commands, and receiving responses.
+    """
+
+    def __init__(self, socket_path="/tmp/pisugar-server.sock"):
+        """
+        Initialize the PiSugarClient with a default socket path.
+
+        :param socket_path: Path to the UNIX socket for PiSugar server.
+        """
+        self.socket_path = socket_path
+
+    def send_command(self, command):
+        """
+        Method to send a command to the PiSugar server.
+
+        :param command: The command string to be sent to the server.
+        :return: The response from the server as a string, or None if an error occurred.
+        """
+        try:
+            # Creating a UNIX socket
+            with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
+                logger.info("Connecting to %s", self.socket_path)
+                # Connect to the server using the socket path
+                sock.connect(self.socket_path)
+
+                # Clear existing socket data
+                sock.settimeout(0.1)
+                logger.info("Clearing socket")
+
+                try:
+                    while True:
+                        data = sock.recv(1024)
+                        if not data:
+                            break
+                        logger.debug("Cleared: %s", data)
+                except socket.timeout:
+                    # Log when socket is cleared
+                    logger.info("Socket clear")
+
+                # Send command
+                logger.info("Sending: %s", command)
+                sock.sendall(command.encode() + b'\n')
+
+                # Get response
+                response = sock.recv(1024)
+                logger.info("Received: %s", response.decode())
+
+                return response.decode()
+        except socket.error as e:
+            # Log any socket errors
+            logger.error("Command failed: %s", e)
+            return None
 
 logger = logging.getLogger(__name__)
 
